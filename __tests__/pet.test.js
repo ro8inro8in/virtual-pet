50bf8981585807994d7dcb1fb18d632c0ab7551a
Robin Edwards <robinedwards1987@gmail.com>
2020-11-04 21:43:16 +0000

    05 Getting Older, Getting Unhealthier

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -27,5 +27,43 @@ describe('constructor', () => {
     
         expect(pet.age).toEqual(1);
   });
-  
+});
+
+describe('constructor', () => {
+  it('has a initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.appetite).toEqual(0);
+  });
+
+});
+
+describe('constructor', () => {
+  it('increase hunger in increments of 5 as pet ages', () => {
+    const pet = new Pet('Fido');
+
+      pet.feed();
+
+    expect(pet.appetite).toEqual(5);
+  });
+
+  });
+
+describe('constructor', () => {
+  it('has a initial fitness level of 10', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
+describe('constructor', () => {
+  it('decrease fitness in increments of 3 as pet ages', () => {
+    const pet = new Pet('Fido');
+
+      pet.gettingOld()
+
+    expect(pet.fitness).toEqual(7);
+  });
+
 });
--- src/pet.js
+++ src/pet.js
@@ -1,17 +1,27 @@
 const pet = new Pet("Fido");
 pet.name;
 
+//constructor functions
 function Pet(name) {
   this.name = name;
   this.age = 0;
-  /*this.growUp = function (){
-    this.age++;
-  }*/
+  this.appetite = 0;
+  this.fitness = 10;
+  
+  
+  }
 Pet.prototype.growUp = function() {
     this.age += 1;
-};
+}
+Pet.prototype.feed = function () {
+    this.appetite += 5;
+  }
+Pet.prototype. gettingOld = function(){
+    this.fitness -= 3;
+}  
 
-};
+
+
 
 
 module.exports = Pet;
