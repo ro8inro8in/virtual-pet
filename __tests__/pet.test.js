49d9ed4f8a07998de3972ecc4a2ef00ac0b0c08f
Robin Edwards <robinedwards1987@gmail.com>
2020-11-04 18:48:18 +0000

    04 Getting old

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,3 +10,22 @@ describe("constructor", () => {
     expect(pet.name).toEqual("Fido");
   });
 });
+
+describe('constructor', () => {
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.age).toEqual(0);
+    });
+});
+
+describe('constructor', () => {
+      it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+        
+        pet.growUp();
+    
+        expect(pet.age).toEqual(1);
+  });
+  
+});
--- src/pet.js
+++ src/pet.js
@@ -3,8 +3,15 @@ pet.name;
 
 function Pet(name) {
   this.name = name;
-}
+  this.age = 0;
+  /*this.growUp = function (){
+    this.age++;
+  }*/
+Pet.prototype.growUp = function() {
+    this.age += 1;
+};
+
+};
 
 
-//function Pet() {}
 module.exports = Pet;
