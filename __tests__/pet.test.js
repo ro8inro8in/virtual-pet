d8b757fca4f8cb655779eda8ddbe6f0396dd8e39
Robin Edwards <robinedwards1987@gmail.com>
2020-11-05 13:45:01 +0000

    06 Getting Older, Geting Unhealthier

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,61 +9,45 @@ describe("constructor", () => {
     const pet = new Pet("Fido");
     expect(pet.name).toEqual("Fido");
   });
-});
 
-describe('constructor', () => {
-    it('has a initial age of 0', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.age).toEqual(0);
-    });
-});
 
-describe('constructor', () => {
-      it('increments the age by 1', () => {
-        const pet = new Pet('Fido');
-        
-        pet.growUp();
-    
-        expect(pet.age).toEqual(1);
+  it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
   });
-});
 
-describe('constructor', () => {
+
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  });
+
   it('has a initial hunger of 0', () => {
     const pet = new Pet('Fido');
-
     expect(pet.appetite).toEqual(0);
   });
 
-});
 
-describe('constructor', () => {
   it('increase hunger in increments of 5 as pet ages', () => {
     const pet = new Pet('Fido');
-
-      pet.feed();
-
+      //pet.growUp = 1
+      pet.feed();  
     expect(pet.appetite).toEqual(5);
   });
 
-  });
 
-describe('constructor', () => {
   it('has a initial fitness level of 10', () => {
     const pet = new Pet('Fido');
-
     expect(pet.fitness).toEqual(10);
   });
-});
 
-describe('constructor', () => {
+
   it('decrease fitness in increments of 3 as pet ages', () => {
-    const pet = new Pet('Fido');
-
-      pet.gettingOld()
-
+    const pet = new Pet('Fido');           
+      pet.gettingOld();
     expect(pet.fitness).toEqual(7);
   });
-
+  
 });
+
--- src/pet.js
+++ src/pet.js
@@ -2,6 +2,7 @@ const pet = new Pet("Fido");
 pet.name;
 
 //constructor functions
+//object Pet 
 function Pet(name) {
   this.name = name;
   this.age = 0;
@@ -9,18 +10,31 @@ function Pet(name) {
   this.fitness = 10;
   
   
+ //methods maybe I need if statements 
   }
-Pet.prototype.growUp = function() {
+Pet.prototype.growUp = function(){
     this.age += 1;
 }
-Pet.prototype.feed = function () {
-    this.appetite += 5;
+  Pet.prototype.feed = function (){
+    //this.appetite += 5;
+  if (this.appetite === this.age){
+      return this.appetite +=5;
   }
-Pet.prototype. gettingOld = function(){
-    this.fitness -= 3;
-}  
+}
+Pet.prototype.gettingOld = function(){
+   // this.fitness -= 3;
+   if (this.fitness !== this.age){
+       return this.fitness -= 3;
+    }
+}
 
 
+
+ 
+
+//if growUp applies to this.age 
+// if appetite is equal to age then + 5 to hunger 
+// 
 
 
 
